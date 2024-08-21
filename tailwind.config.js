4f0bd78ded4341144ddc3a2bdadbdb102b93224b
Ioannis Diamantidis <diamantidis@outlook.com>
2024-08-21 23:22:17 +0200

    Added tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./_drafts/**/*.{html,md}",
+    "./_posts/**/*.{html,md}",
+    "./_includes/**/*.{html,md}",
+    "./_layouts/**/*.{html,md}",
+    "./*.{html,md}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
