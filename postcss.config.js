1487f28f379d575c770d25ed1becd97980b15986
Ioannis Diamantidis <diamantidis@outlook.com>
2026-04-13 23:42:22 +0200

    Remove postcss.config.js and move @apply rules to Tailwind input
    Delete PostCSS config file. Move @layer utilities block with @apply
    directives from _scss/custom.scss to assets/css/input.css since @apply
    only works within the Tailwind CLI build pipeline.

--- postcss.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    plugins: [
-        require('tailwindcss'),
-        ...(process.env.JEKYLL_ENV == 'production'
-            ? [require('cssnano')({ preset: 'default' })]
-            : [])
-    ]
-}
